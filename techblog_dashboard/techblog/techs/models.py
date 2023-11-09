8411bc6bcd3d01e386c21e1fbb39642a3fcf27b0
es3442 <es3442@naver.com>
2023-11-10 03:10:25 +0900

    회원가입, 로그인, csv->DB 저장 구현

--- techblog_dashboard/techblog/techs/models.py
+++ techblog_dashboard/techblog/techs/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 like_list=[]
 
 class Company(models.Model):
-    company_name = models.CharField(max_length=200, verbose_name='회사명')
+    company_name = models.CharField(max_length=30, verbose_name='회사명', unique=True)
 
     def __str__(self):
         return f'{self.company_name}'
@@ -18,7 +18,7 @@ class Post(models.Model):
     date = models.DateTimeField(verbose_name='작성일')
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    url = models.URLField(max_length=200, verbose_name='링크')
+    url = models.TextField(verbose_name='링크')
 
     def __str__(self):
         return f'{self.title},{self.company.company_name},{self.date},{self.views},{self.likes}'
@@ -26,7 +26,7 @@ class Post(models.Model):
 
 class Tag(models.Model):
     # tag_id pk
-    tag_name = models.CharField(max_length=200, verbose_name='태그 내용')
+    tag_name = models.CharField(max_length=200, verbose_name='태그 내용', unique=True)
 
     def __str__(self):
         return f'{self.tag_name}'
@@ -54,3 +54,22 @@ class Company_Tag(models.Model):
 
     def __str__(self):
         return f'{self.company.company_name},{self.tag.tag_name}'
+
+
+# class User(models.Model): 내장된 User 모델이다.
+#     # user_id 
+#     user_email=models.CharField(max_length=50, verbose_name='이메일주소')
+#     user_password=models.CharField(max_length=20, verbose_name='패스워드')
+
+#     def __str__(self):
+#         return f'{self.user_email},{self.user_password}'
+
+class User_Likes(models.Model):
+    # user_likes_id  pk
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE,
+                            verbose_name='사용자')  # class Post# tag table
+    post_id=models.ForeignKey(Post, on_delete=models.CASCADE,
+                            verbose_name='게시글')  # class Post# tag table
+
+    def __str__(self):
+        return f'{self.user_id.username},{self.post_id.title}'
